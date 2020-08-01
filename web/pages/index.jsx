a8cf02e9634fd9e1afff8586cbd5f2a414b23f42
Radhemohan Yadav <48642948+radhemky@users.noreply.github.com>
2020-08-01 19:20:03 +0530

    🔗 Updated boilerplate links for listed videos 🔗
    Links are temporary.

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import {useSession} from 'next-auth/client';
 import Layout from '../layout';
+import ReactPlayer from 'react-player'
+
+
+const list = ['https://www.youtube.com/watch?v=ysz5S6PUM-U', 'https://www.youtube.com/watch?v=T82aYJsDhqA', 'https://www.youtube.com/watch?v=Cx1qHKE_y9Q', 'https://www.youtube.com/watch?v=bcw88YverlE', 'https://www.youtube.com/watch?v=suTYbf-FtXg'];
+
 
 const Home = () => {
     const [session, loading] = useSession();
-    return <Layout>{loading ? <h3>Loading</h3> : <h3>Home</h3>}</Layout>;
+    return (
+    <Layout>{loading ? <h3>Loading</h3> : 
+     <ul>
+        {list.map(function(item) {
+           return <ReactPlayer url= {item} style={{padding: 50}} />;
+        })}
+    </ul> 
+    }
+    </Layout>
+    );
 };
 
 export default Home;
