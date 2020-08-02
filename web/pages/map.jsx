444f3e77e0c45c07ed8bf348a0359f68eab50461
Radhemohan Yadav <48642948+radhemky@users.noreply.github.com>
2020-08-02 16:13:03 +0530

    Created map component for iterating the marker.
    It's just a placeholder file. the code isn't stable yet!

---
+++ web/pages/map.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Layout from '../layout';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import dynamic from 'next/dynamic';
+
+const position = [51.505, -0.09];
+
+const DynamicComponentWithNoSSR = dynamic (
+    () =>         
+    <Map center={position} zoom={13}>
+    <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+    <Marker position={position}>
+    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+    </Marker>
+    </Map>,
+    { ssr: false }
+)
+
+const MapDis = () => {
+
+return(
+        <DynamicComponentWithNoSSR/>
+    )
+      
+};
+
+export default MapDis;
+
