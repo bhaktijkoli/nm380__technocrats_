10db4816db14bc1447ab2abfe8394ccfedc6b5c9
bhaktijkoli <bhaktijkoli121@gmail.com>
2020-08-03 14:37:45 +0530

    Added Open Street Map

--- app/app/screens/Play/MapView.jsx
+++ app/app/screens/Play/MapView.jsx
@@ -1,52 +1,63 @@
-import React, { useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView, { Marker, Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useState, useRef } from 'react';
+import ModalSelector from 'react-native-modal-selector'
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import MapView, { Marker, Polyline, OverlayComponent, UrlTile, MAP_TYPES, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Image } from 'react-native-animatable';
+
 
 export default (props) => {
     let locations = props.locations
     if (locations.length === 0) return null;
 
+    const [provider, setProvider] = useState('google');
+
     const cameraRef = useRef(null);
+    const mapLayerRef = useRef(null);
 
-    if (cameraRef.current !== null) {
+    const mapLayerButton = (
+        <TouchableOpacity activeOpacity={0.8} style={{ position: 'absolute', top: 10, right: 10, padding: 3, backgroundColor: '#fff' }} onPress={e => mapLayerRef.current.open()}>
+            <Image source={require('./../../assets/globe.png')} style={{ width: 28, height: 28 }} />
+        </TouchableOpacity>
+    )
 
-    }
 
     const currentLocation = props.currentLocation;
 
     return (
-        <MapView
-            provider={PROVIDER_GOOGLE}
-            style={{ ...StyleSheet.absoluteFillObject }}
-            loadingEnabled={true}
-            initialCamera={{
-                center: {
-                    latitude: props.locations[0].lat,
-                    longitude: props.locations[0].lng,
-                },
-                altitude: props.locations[0].alt,
-                heading: props.locations[0].heading,
-                zoom: 18,
-                pitch: 10,
-            }}
-            zoomEnabled={true}
-        >
+        <React.Fragment>
+            <MapView
+                provider={provider === 'google' ? PROVIDER_GOOGLE : null}
+                mapType={provider === 'google' ? MAP_TYPES.STANDARD : MAP_TYPES.NONE}
+                style={{ ...StyleSheet.absoluteFillObject }}
+                loadingEnabled={true}
+                initialCamera={{
+                    center: {
+                        latitude: props.locations[0].lat,
+                        longitude: props.locations[0].lng,
+                    },
+                    altitude: props.locations[0].alt,
+                    heading: props.locations[0].heading,
+                    zoom: 18,
+                    pitch: 10,
+                }}
+                zoomEnabled={true}
+            >
 
-            {/* {Current Position Marker} */}
-            {
-                currentLocation === null ? null :
-                    <Marker
-                        title="Current Position"
-                        pinColor="red"
-                        coordinate={{
-                            latitude: currentLocation.lat,
-                            longitude: currentLocation.lng
-                        }}
-                    />
-            }
+                {/* {Current Position Marker} */}
+                {
+                    currentLocation === null ? null :
+                        <Marker
+                            title="Current Position"
+                            pinColor="red"
+                            coordinate={{
+                                latitude: currentLocation.lat,
+                                longitude: currentLocation.lng
+                            }}
+                        />
+                }
 
-            {/* {Start Marker} */}
-            {/* <Marker
+                {/* {Start Marker} */}
+                {/* <Marker
                 title="Start"
                 pinColor="blue"
                 coordinate={{
@@ -55,8 +66,8 @@ export default (props) => {
                 }}
             /> */}
 
-            {/* {Stop Marker} */}
-            {/* <Marker
+                {/* {Stop Marker} */}
+                {/* <Marker
                 title="Stop"
                 pinColor="green"
                 coordinate={{
@@ -65,31 +76,58 @@ export default (props) => {
                 }}
             /> */}
 
-            {/* Lines */}
-            {
-                locations.map((location, key) => {
-                    if (key === locations.length - 1) return null;
-                    if (location.type === 'pause') return null;
-                    if (key > 1) {
-                        if (locations[key - 1].type === 'pause') return null
-                    }
-                    return (
-                        <Polyline
-                            coordinates={[
-                                { latitude: location.lat, longitude: location.lng },
-                                { latitude: locations[key + 1].lat, longitude: locations[key + 1].lng },
-                            ]}
-                            strokeColor="#000"
-                            strokeWidth={6}
-                            tappable={true}
-                            onPress={e => {
-                                props.onCurrentLocationChange(location.elapsed / 1000);
-                            }}
-                        />
-                    )
-                })
-            }
+                {/* Lines */}
+                {
+                    locations.map((location, key) => {
+                        if (key === locations.length - 1) return null;
+                        if (location.type === 'pause') return null;
+                        if (key > 1) {
+                            if (locations[key - 1].type === 'pause') return null
+                        }
+                        return (
+                            <Polyline
+                                coordinates={[
+                                    { latitude: location.lat, longitude: location.lng },
+                                    { latitude: locations[key + 1].lat, longitude: locations[key + 1].lng },
+                                ]}
+                                strokeColor="#000"
+                                strokeWidth={6}
+                                tappable={true}
+                                onPress={e => {
+                                    props.onCurrentLocationChange(location.elapsed / 1000);
+                                }}
+                                zIndex={10}
+                            />
+                        )
+                    })
+                }
 
-        </MapView>
+                {
+                    provider !== 'google' ?
+                        <UrlTile
+                            urlTemplate="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            maximumZ={19}
+                            zIndex={-1}
+                            flipY={false}
+                        />
+                        : null
+                }
+                {/* <OverlayComponent
+                style={{ position: "absolute", top: 10, right: 10 }}
+                image={{ uri: "https://img.icons8.com/color/48/000000/globe--v1.png" }}
+            /> */}
+
+            </MapView>
+            <ModalSelector
+                ref={mapLayerRef}
+                customSelector={mapLayerButton}
+                data={[
+                    { key: 1, section: true, label: "Select Map Layer" },
+                    { key: 2, label: 'Google', value: 'google' },
+                    { key: 3, label: 'Open Street Maps', value: 'osm' },
+                ]}
+                onChange={(option) => { setProvider(option.value) }} />
+        </React.Fragment>
+
     )
 }
